8ad9bbc22edee267811de358d0cebbf135ee372b
Balder <balder.ansgar@gmail.com>
2026-03-02 16:24:41 -0300

    add classe "Livro"

---
+++ biblioteca-poo/livro.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from item_biblioteca import ItemBiblioteca as ib
+
+class Livro(ib):
+    def __init__(self, codigo, titulo, ano, disponivel, autor, num_paginas):
+        super().__init__(codigo, titulo, ano, disponivel)
+        self.__autor = autor
+        self.__num_paginas = num_paginas
+
+    def exibir_detalhes(self):
+        print(f"Livro -> Código: {self.__codigo} | Título: {self.__titulo} | Ano: {self.__ano} \nAutor: {self.__autor} | Número de paginas: {self.__num_paginas} | Disponível: {self.__disponivel}")
